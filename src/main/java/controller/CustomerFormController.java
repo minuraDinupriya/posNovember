4c9b63d32f7ed631680504f144c5aa46e48ccdab
Minura Dinupriya <mranaweera793@gmail.com>
2023-11-30 09:32:23 +0530

    set delete button

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -2,31 +2,28 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD
-=======
-import javafx.scene.control.Alert;
->>>>>>> 640e532 (sql connected)
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+
+import javafx.scene.control.*;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import lombok.SneakyThrows;
+import model.tm.CustomerTm;
 
 import java.net.URL;
-<<<<<<< HEAD
-=======
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
->>>>>>> 640e532 (sql connected)
+
+import java.sql.*;
+
 import java.util.ResourceBundle;
 
 public class CustomerFormController implements Initializable {
 
     @FXML
-    private TableView customerTable;
+    private TableView<CustomerTm> customerTable;
 
     @FXML
     private TableColumn idCol;
@@ -73,25 +70,22 @@ public class CustomerFormController implements Initializable {
     }
 
     @FXML
-    void reloadBtnOnAction(ActionEvent event) {
-
+    void reloadBtnOnAction(ActionEvent event) throws SQLException, ClassNotFoundException {
+        reloadCustomerTable();
     }
 
     @FXML
-<<<<<<< HEAD
-    void saveBtnOnAction(ActionEvent event) {
-
-=======
     void saveBtnOnAction(ActionEvent event) throws ClassNotFoundException, SQLException {
         String sql="INSERT INTO customer VALUES('"+customerIdField.getText()+"','"+customerNameField.getText()+"','"+addressTxtField.getText()+"','"+Double.parseDouble(salaryTxtField.getText())+"')";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "minura");
         Statement statement = connection.createStatement();
         int number = statement.executeUpdate(sql);
+        reloadCustomerTable();
         if (number>0){
             new Alert(Alert.AlertType.INFORMATION,"Update Success").show();
         }
->>>>>>> 640e532 (sql connected)
+        connection.close();
     }
 
     @FXML
@@ -99,8 +93,64 @@ public class CustomerFormController implements Initializable {
 
     }
 
+    @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        reloadCustomerTable();
+    }
 
+    public void deleteBtnOnAction(String id) throws SQLException, ClassNotFoundException{
+        String deleteQuery="DELETE FROM customer WHERE id='"+id+"'";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "minura");
+        Statement statement = connection.createStatement();
+        int number = statement.executeUpdate(deleteQuery);
+        reloadCustomerTable();
+        //customerTable.refresh();   just like reloadCustomerTable()
+        if (number>0){
+            new Alert(Alert.AlertType.INFORMATION,"Deleted Successfully").show();
+        }else {
+            new Alert(Alert.AlertType.INFORMATION,"Something went wrong").show();
+        }
+        connection.close();
+    }
+
+    private void reloadCustomerTable() throws ClassNotFoundException, SQLException  {
+        ObservableList<CustomerTm> customerTmList = FXCollections.observableArrayList();
+        String selectQuery="SELECT * FROM customer";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "minura");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(selectQuery);
+        while (resultSet.next()){
+            Button button=new Button("Delete");
+            CustomerTm customerTm=new CustomerTm(resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDouble(4),
+                    button
+            );
+
+            button.setOnAction(actionEvent -> {
+                try {
+                    deleteBtnOnAction(customerTm.getId());
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            customerTmList.add(customerTm);
+        }
+        connection.close();
+
+        customerTable.setItems(customerTmList);
+
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        salaryCol.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        optionCol.setCellValueFactory(new PropertyValueFactory<>("optionBtn"));
     }
 }
