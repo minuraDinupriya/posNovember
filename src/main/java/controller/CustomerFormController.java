5b9c403c3742c954fa02c5f1dcf99ed9754ae7f1
Minura Dinupriya <mranaweera793@gmail.com>
2023-11-30 12:11:08 +0530

    customer view done

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -22,6 +22,9 @@ import java.util.ResourceBundle;
 
 public class CustomerFormController implements Initializable {
 
+    public JFXButton clearBtn;
+    public JFXTextField idTxtField;
+    public JFXTextField nameTxtField;
     @FXML
     private TableView<CustomerTm> customerTable;
 
@@ -46,8 +49,6 @@ public class CustomerFormController implements Initializable {
     @FXML
     private JFXButton saveBtn;
 
-    @FXML
-    private JFXTextField customerIdField;
 
     @FXML
     private JFXTextField addressTxtField;
@@ -62,9 +63,6 @@ public class CustomerFormController implements Initializable {
     private Label customerSalaryField;
 
     @FXML
-    private JFXTextField customerNameField;
-
-    @FXML
     void customerTableOnAction(ActionEvent event) {
 
     }
@@ -76,11 +74,30 @@ public class CustomerFormController implements Initializable {
 
     @FXML
     void saveBtnOnAction(ActionEvent event) throws ClassNotFoundException, SQLException {
-        String sql="INSERT INTO customer VALUES('"+customerIdField.getText()+"','"+customerNameField.getText()+"','"+addressTxtField.getText()+"','"+Double.parseDouble(salaryTxtField.getText())+"')";
+        String sql="INSERT INTO customer VALUES('"+idTxtField.getText()+"','"+nameTxtField.getText()+"','"+addressTxtField.getText()+"','"+Double.parseDouble(salaryTxtField.getText())+"')";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "minura");
+        try {
+            Statement statement = connection.createStatement();
+            int number = statement.executeUpdate(sql);
+            reloadCustomerTable();
+            if (number>0){
+                new Alert(Alert.AlertType.INFORMATION,"Update Success").show();
+                clear();
+            }
+        }catch (SQLIntegrityConstraintViolationException e){
+            new Alert(Alert.AlertType.INFORMATION,"Duplicate entry").show();
+        }
+        connection.close();
+    }
+
+    @FXML
+    void updateBtnOnAction(ActionEvent event) throws ClassNotFoundException, SQLException  {
+        String updateQuery="UPDATE customer SET name='"+nameTxtField.getText()+"',address='"+addressTxtField.getText()+"',salary="+Double.parseDouble(salaryTxtField.getText())+" WHERE id='"+idTxtField.getText()+"'";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "minura");
         Statement statement = connection.createStatement();
-        int number = statement.executeUpdate(sql);
+        int number = statement.executeUpdate(updateQuery);
         reloadCustomerTable();
         if (number>0){
             new Alert(Alert.AlertType.INFORMATION,"Update Success").show();
@@ -88,15 +105,20 @@ public class CustomerFormController implements Initializable {
         connection.close();
     }
 
-    @FXML
-    void updateBtnOnAction(ActionEvent event) {
-
-    }
-
     @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        customerTable.getSelectionModel().selectedItemProperty().addListener((observableValue, customerTm, newCustomerTmValue) -> fillTextFields(newCustomerTmValue));
         reloadCustomerTable();
+    }
+
+    private void fillTextFields(CustomerTm customerTm) {
+        if (customerTm != null) {
+            idTxtField.setText(customerTm.getId());
+            nameTxtField.setText(customerTm.getName());
+            addressTxtField.setText(customerTm.getAddress());
+            salaryTxtField.setText(customerTm.getSalary() + "");
+        }
     }
 
     public void deleteBtnOnAction(String id) throws SQLException, ClassNotFoundException{
@@ -134,9 +156,7 @@ public class CustomerFormController implements Initializable {
             button.setOnAction(actionEvent -> {
                 try {
                     deleteBtnOnAction(customerTm.getId());
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                } catch (ClassNotFoundException e) {
+                } catch (SQLException | ClassNotFoundException e) {
                     throw new RuntimeException(e);
                 }
             });
@@ -152,5 +172,16 @@ public class CustomerFormController implements Initializable {
         addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
         salaryCol.setCellValueFactory(new PropertyValueFactory<>("salary"));
         optionCol.setCellValueFactory(new PropertyValueFactory<>("optionBtn"));
+    }
+
+    public void clearBtnOnAction(ActionEvent actionEvent) {
+        clear();
+    }
+
+    private void clear() {
+        idTxtField.clear();
+        nameTxtField.clear();
+        addressTxtField.clear();
+        salaryTxtField.clear();
     }
 }
